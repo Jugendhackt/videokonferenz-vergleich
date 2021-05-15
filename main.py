af151c278c4d07464a22ac22cd9bcc57f8648e32
Konradrundfunk <33392939+Konradrundfunk@users.noreply.github.com>
2021-05-15 11:10:56 +0200

    engine

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/api", )
+def api():
+    return("api")
+    
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
